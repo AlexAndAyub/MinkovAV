702723c4567284f983c3d66cb2da4177a57d1afc
AlexAndAyub <142718723+AlexAndAyub@users.noreply.github.com>
2023-11-06 17:33:13 +0500

    Update find_string_in_text.py

--- PYTHON/find_string_in_text.py
+++ PYTHON/find_string_in_text.py
@@ -4,7 +4,8 @@ class FindStringInText:
     def __init__(self, word: str):
         self.word = word
         self.regex_expression = self.regex(word)
-        self.result = []
+        self.string_number = 1
+        self.result = {}
 
     def regex(self, word: str) -> str:
         regex = "^"
@@ -13,20 +14,28 @@ class FindStringInText:
         return regex + ".*$"
 
     def find_string(self, string: str) -> list:
-        self.result.append(re.findall(self.regex_expression, string.lower()))
+        _ = re.findall(self.regex_expression, string.lower())
+        if _:
+            self.result[self.string_number] = _[0]
+        self.string_number += 1
+        return self.result
+
+    def mark_simbol(self) -> str:
+        for key in self.result:
+            for _ in self.word:
+                self.result[key] = re.sub(_, f'[{_.upper()}]', self.result[key])  
         return self.result
 
     def __str__(self):
         return f'{self.result}'
-    
 
 word = 'minkov'
 result = FindStringInText(word)
-i=1
 
 with open('text.txt', 'r') as f:
     for line in f.readlines():
-        result.find_string(f'{i}  {line}')
-        i+=1
+        result.find_string(line)
 
 print(result)
+print()
+print(result.mark_simbol())
